b354000d83cf8b8540a389214b9dbc112820ede7
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-16 15:38:19 -0700

    array intersection tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,34 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil
+    return []
+  end
+
+  i = 0
+  while i < array1.length
+    k = 0
+    match = false
+    while k < array2.length && match == false
+      if array1[i] == array2[k]
+         match = true
+      end
+      k += 1
+    end
+
+    if match != true
+      array1.delete_at(i)
+    else
+      i += 1
+    end
+
+  end
+
+  array1
 end
+
+
+# Loop through each element in array1 and compare each of them to each element
+# in array 2. This means having a nested while loop to go through each element in array 2.
+# If no match is found between the array1 element and all of the array 2 elements then the element
+# should be deleted from the array. If the element is kept then it should be sorted against all of
+# the preceding kept elements.
